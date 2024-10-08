25234604db1a4afe3a9e5df8f7c39f3d78b8a645
nahom <nahomdev2021@gmail.com>
2024-10-08 14:23:09 +0300

    first commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://menu-api-o2y8.onrender.com/',
 });
 
 export const fetchMenuItems = async () => {
